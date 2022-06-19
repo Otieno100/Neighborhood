93bbe364e3315d7d33ba5eab49f9d400a155b5b5
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-19 22:52:27 +0300

    removed welcome.html

--- hood/urls.py
+++ hood/urls.py
@@ -1,6 +1,14 @@
 from django.urls import re_path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    re_path('^$',views.welcome,name = 'welcome'),
+    # re_path('^$',views.welcome,name = 'welcome')
+    re_path(r'^$',views.single_neighbourhood,name='hood'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
