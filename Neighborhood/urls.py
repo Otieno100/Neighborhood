14e1cdaeee9cfb150ea878ee255cac487973fc0e
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-19 20:15:09 +0300

    created a project neighborhood

---
+++ Neighborhood/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import include, re_path
+
+urlpatterns = [
+    re_path('admin/', admin.site.urls),
+    re_path(r'^hood/',include('hood.urls'))
+]
+
