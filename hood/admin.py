8aae5d87c4678c45a612a9e1916cd67d933ae5b9
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-19 20:40:32 +0300

    registered my models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Neighbourhood, Business,Profile,Post
+
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
+admin.site.register(Profile)
