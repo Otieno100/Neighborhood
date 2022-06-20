70c1980838669ce29ce0f41f4de682e19a3c55c3
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-20 15:21:57 +0300

    created Neighbnourhood form

---
+++ hood/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Post, Neighbourhood, Profile, Business
+
+class UploadPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['publish_date']
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        
+class UploadBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user']
+
+
+
+class NewNeighbourhoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ['editor', 'pub_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }        
